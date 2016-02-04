7a0455f25398b75a2996e38edbc4b2deb791bb22
Zopper9000 <david.lu@hotmail.com>
2016-02-04 01:49:51 -0500

    added functions

--- kernel.cu
+++ kernel.cu
@@ -1,121 +1,117 @@
+//N-Body Physics Simulation
+//Randomly generates and displays an N-Body system
+//Code from Nvidia's GPU Gems 3 Chapter 31
 
+#include "cuda_runtime_api.h"
 #include "cuda_runtime.h"
+#include "cuda.h"
+#include "device_functions.h"
 #include "device_launch_parameters.h"
 
 #include <stdio.h>
+#include <cmath>
 
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+//N, number of bodies
+const unsigned int N = 100;
 
-__global__ void addKernel(int *c, const int *a, const int *b)
-{
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
+//p, number of blocks
+const unsigned int p = 5;
+
+//Interaction between two bodies
+//Integrate using leapfrog-Verlet integrator
+//a ~= G * summation(1, N, m[j]*r[i][j] / (r^2 + E^2)^3/2
+//G = 1 for simplicity
+__device__ float3 bobyBodyInteraction(float4 b1, float4 b2, float3 a1) {
+	//Distance between bodies
+	float3 d;
+	//3 FLOPS
+	d.x = b2.x - b1.x;
+	d.y = b2.y - b1.y;
+	d.z = b2.z - b1.z;
+
+	//Square distance
+	//6 FLOPS
+	float square = d.x * d.x + d.y * d.y + d.z * d.z;
+
+	//Cube
+	//2 FLOPS
+	float cube = square * square * square;
+
+	//Invert and sqrt
+	//2 FLOPS
+	float invert = 1.0f / sqrtf(cube);
+
+	//Calculate s
+	float s = b2.w * invert;
+
+
+	//Calculate a
+	a1.x += d.x * s;
+	a1.y += d.y * s;
+	a1.z += d.z * s;
+	return a1;
+
+}
+
+//Calculate all interactions within a tile
+__device__ float3 tile_calculation(float4 pos, float3 a) {
+	int i;
+	//Used shared memory
+	extern __shared__ float4 sPos[];
+	for (i = 0; i < blockDim.x; i++) {
+		a = bobyBodyInteraction(pos, sPos[i], a);
+	}
+	return a;
+}
+
+//Calculate acceleration for p bodies with p threads resulting from N interactions
+__global__ void calculate_forces(void *devX, void *devA) {
+	//Declare shared position
+	extern __shared__ float4 sPos[];
+
+	//Get from memory
+	float4 *globalX = (float4 *)devX;
+	float4 *globalA = (float4 *)devA;
+
+	//Initialize position
+	float4 pos;
+
+	//Initialize variables
+	int i, tile;
+	float3 acc = { 0.0f, 0.0f, 0.0f };
+
+	//Get index
+	int getId = blockIdx.x * blockDim.x + threadIdx.x;
+
+	//Get position
+	pos = globalX[getId];
+
+	//Calculate N bodies
+	for (i = 0, tile = 0; i < N; i += p, tile++) {
+		//Get index
+		int idx = tile * blockDim.x + threadIdx.x;
+
+		//Update position
+		sPos[threadIdx.x] = globalX[idx];
+
+		//Barrier
+		__syncthreads();
+
+		//Get acceleration
+		acc = tile_calculation(pos, acc);
+
+		//Barrier
+		__syncthreads();
+	}
+
+	//Save in global memory
+	float4 acc4 = { acc.x, acc.y, acc.z, 0.0f };
+	globalA[getId] = acc4;
+
 }
 
 int main()
 {
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
-
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
-
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
-
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
 
     return 0;
-}
-
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-{
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
-
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
-
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
-
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
-
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
 }
